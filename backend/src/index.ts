40dc948e9fa8719c9d13c3746e765a1fd35345b1
Seyf <seyfettin.baskara@gmail.com>
2022-06-21 20:06:10 +0200

    add cors package

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express'
 import connectDB from '../database/db'
 import taskRouter from '../routers/taskRouter'
+import cors from 'cors'
 
 connectDB()
 const app: Express = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/tasks', taskRouter)
 
